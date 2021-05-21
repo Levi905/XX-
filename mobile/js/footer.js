7bc7438e4b631c9ef186d985c2c4280b35ce7642
Levi905 <507816093@qq.com>
2021-05-21 17:37:58 +0800

    还差最后一个移动端页面

---
+++ mobile/js/footer.js
@@ -0,0 +1,12 @@
+document.write(`<div id="footer">
+<ul>
+    <li><i class="bi bi-receipt"></i><br><span><a href="">首页</a></span>
+    </li>
+    <li><i class="bi bi-play-btn"></i><br><span><a href="">视频</a></span>
+    </li>
+    <li><i class="bi bi-menu-up"></i><br><span><a href="">详情</a></span>
+    </li>
+    <li><i class="bi bi-person"></i><br><span><a href="">我的</a></span>
+    </li>
+</ul>
+</div>`);
